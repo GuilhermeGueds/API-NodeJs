3bb7da98ff64f860082fb1123dfc2f2bdd9d6571
Guedes <gguilhermegguedes@gmail.com>
2022-04-06 16:56:06 -0300

    cofig debug + create + list

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import express from "express";
 
-import { createCourse } from "./routes";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/categories/", categoriesRoutes);
 
-app.get("/", createCourse) 
-    
 
 app.listen(3333);
 
