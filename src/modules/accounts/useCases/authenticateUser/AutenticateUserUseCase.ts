b4a37c063de76ee2ded1bbd4f99ff003a95894da
Guedes <gguilhermegguedes@gmail.com>
2022-05-10 10:10:01 -0300

    Cappturado e tratado os errors

---
+++ src/modules/accounts/useCases/authenticateUser/AutenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { sign } from "jsonwebtoken";
+import { AppError } from "../../../../errors/AppError";
+
+interface IRequest {
+    email: string;
+    passworld: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+    async execute({email, passworld}: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user){
+            throw new AppError("Email or passworld incorrect !");
+        }
+
+        const passworldMath = await compare(passworld, user.passworld);
+
+        if (!passworldMath){
+            throw new AppError("Email or passworld incorrect !");
+        }
+
+        const token = sign({}, "c8d9b07a8cfef99bdab6c49a02dfb69b", {
+           subject: user.id,
+           expiresIn: "1d" 
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        };
+        return tokenReturn;
+    }
+}
+
+
+export { AuthenticateUserUseCase };
